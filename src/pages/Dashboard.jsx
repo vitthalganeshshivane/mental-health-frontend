622e7469038a474c1c9e7f4a873e0195417f8386
vitthal4545 <vitthalgs1434@gmail.com>
2025-09-06 17:25:59 +0530

    Dashboard Page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  // You'll implement survey form here in detail later
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Welcome to MindCare Dashboard</h2>
+      <p>Survey form will go here.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
