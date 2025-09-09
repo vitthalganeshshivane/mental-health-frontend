2d74c82c965502cb10083e8d0e7c1f155e7b7307
vitthal4545 <vitthalgs1434@gmail.com>
2025-09-09 14:55:51 +0530

    Updated the routes from the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { Toaster } from "react-hot-toast";
+
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import HomeRedirector from "./components/HomeRedirector";
+import SurveyForm from "./components/SurveyForm";
 
 function App() {
   return (
     <Router>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<HomeRedirector />} />
+        <Route path="/home" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/survey-form"
+          element={
+            <PrivateRoute>
+              <SurveyForm />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
